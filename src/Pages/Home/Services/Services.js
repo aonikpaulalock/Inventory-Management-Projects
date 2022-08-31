2fd638249f1be366c73e201aa155f965619d5cef
aonikpaulalock <alockpaul9845@gmail.com>
2022-08-31 17:16:02 +0600

    update service section text

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -3,9 +3,9 @@ import Service from './Service';
 // https://i.ibb.co/bmQxM89/Dolar-1.png
 // https://i.ibb.co/r6vSX4S/Service-2.png
 const services = [
-  { id: 1, name: "FREE SHIPPING", des: "The proper business solution for your developing business strategies and corporation", img: "https://i.ibb.co/T2H2r8D/Service-3.png" },
-  { id: 2, name: "FREE DELIVERY", des: "The proper business solution for your developing business strategies and corporation", img: "https://i.ibb.co/z58r4M1/Service-1.png" },
-  { id: 3, name: "PRICE GUARANTEE", des: "The proper business solution for your developing business strategies and corporation", img: "https://i.ibb.co/bmQxM89/Dolar-1.png" },
+  { id: 1, name: "Free Shipping", des: "The proper business solution for your developing business strategies and corporation", img: "https://i.ibb.co/T2H2r8D/Service-3.png" },
+  { id: 2, name: "Free Delivery", des: "The proper business solution for your developing business strategies and corporation", img: "https://i.ibb.co/z58r4M1/Service-1.png" },
+  { id: 3, name: "Price Guarantee", des: "The proper business solution for your developing business strategies and corporation", img: "https://i.ibb.co/bmQxM89/Dolar-1.png" },
 ]
 
 const Services = () => {
